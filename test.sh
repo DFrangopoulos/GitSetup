5a8ad83b928927b4b15d80c905057f4f5014a452
Dionyssios Frangopoulos <e@dnf.gr>
2020-07-13 16:44:53 +0300

    Add files via upload

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ touch /etc/lighttpd/lighttpd_gitview.conf
 config_view=/etc/lighttpd/lighttpd_gitview.conf
 
 
-read -r -d '' view_contents << EOM
+read -d '' view_contents << EOM
 
 server.document-root = "/home/git_repo/index.html/"
 server.port = 3000
@@ -27,13 +27,14 @@ server.modules += ( "mod_alias", "mod_cgi", "mod_redirect", "mod_setenv" )
 url.redirect += ( "^/gitweb$" => "/gitweb/" )
 alias.url += ( "/gitweb/" => "/usr/share/gitweb/" )
 
-$HTTP["url"] =~ "^/gitweb/" {
-       setenv.add-environment = (
-               "GITWEB_CONFIG" => "/etc/gitweb.conf",
-               "PATH" => env.PATH
-       )
-       cgi.assign = ( ".cgi" => "" )
-       server.indexfiles = ( "gitweb.cgi" )
+\$HTTP["url"] =~ "^/gitweb/" {
+	setenv.add-environment = (
+		"GITWEB_CONFIG" => "/etc/gitweb.conf",
+		"PATH" => env.PATH
+	)
+
+	cgi.assign = ( ".cgi" => "" )
+	server.indexfiles = ( "gitweb.cgi" )
 }
 
 EOM
